dedf9da0a8e723c657ab28e09db93a9cabd95310
Виталий <46228376+LayXL@users.noreply.github.com>
2024-06-14 00:14:43 +0500

    feat: vite cfg port 8080

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: { port: 80 },
+    server: { port: 8080 },
     plugins: [react()],
 })

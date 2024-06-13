dc3a7bc9c3a8df825ebdec11a771caba54e0efb3
Виталий <46228376+LayXL@users.noreply.github.com>
2024-06-14 00:17:38 +0500

    feat: added iframe for testing

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,7 @@
 export const Home = () => {
-    return <div></div>
+    return (
+        <div>
+            <iframe width={380} height={740} src="http://localhost:5173/" />
+        </div>
+    )
 }

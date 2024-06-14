664a1012f6d75b6daef02353f4e87d483600dd7d
Виталий <46228376+LayXL@users.noreply.github.com>
2024-06-14 14:15:04 +0500

    perf: Removed import "@telegram-apps/telegram-ui/dist/styles.css"

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,4 @@
 import { AppRoot } from "@telegram-apps/telegram-ui"
-import "@telegram-apps/telegram-ui/dist/styles.css"
 import { StrictMode } from "react"
 import ReactDOM from "react-dom/client"
 import { RouterProvider, createBrowserRouter } from "react-router-dom"

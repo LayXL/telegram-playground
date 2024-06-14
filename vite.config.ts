f11d58e07c0d47e07c84e4f24f2496f1f2b0abac
Виталий <46228376+LayXL@users.noreply.github.com>
2024-06-14 13:12:43 +0500

    chore: Added @ path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import react from "@vitejs/plugin-react"
+import path from "node:path"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
     server: { port: 8080 },
     plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 })

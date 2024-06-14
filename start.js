d586af0031a165bbd43d4eff2e2652850367bfe2
Виталий <46228376+LayXL@users.noreply.github.com>
2024-06-14 17:25:35 +0500

    fix: Server setup fix

--- start.js
+++ start.js
@@ -1,11 +1,19 @@
 #!/usr/bin/env node
+import react from "@vitejs/plugin-react"
+import path from "node:path"
 import { fileURLToPath } from "node:url"
 import { createServer } from "vite"
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url))
 
 const server = await createServer({
-    configFile: "vite.config.ts",
+    server: { port: 8080 },
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
     root: __dirname,
 })
 await server.listen()
